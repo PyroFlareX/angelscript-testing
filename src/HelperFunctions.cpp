9ac8656f40c58566e78aba0b0acf01de90a385c2
PyroFlareX <31327816+PyroFlareX@users.noreply.github.com>
2021-09-24 09:56:18 -0700

    Set up finished for testing

--- src/HelperFunctions.cpp
+++ src/HelperFunctions.cpp
@@ -1,10 +1,12 @@
 #include "HelperFunctions.h"
 
+#include <iostream>
+
 namespace scrpt
 {
 	void function()
 	{
-		
+		std::cout << "Test Function!\n";
 	}
 	
 		
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,12 @@
-#include "HelperFunctions.h"
-
 #include <iostream>
+
+#include "HelperFunctions.h"
 
 int main()
 {
 	std::cout << "Output\n";
+
+	scrpt::function();
 
 	return 0;
 }
